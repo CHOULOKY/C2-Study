413940162b17a536d3a6769b2ebcd65f6433dcf4
CHOULOKY <dlsgml5131@gmail.com>
2024-12-23 22:21:20 +0900

    CHOULOKY BAEKJOON
    24.12.23

--- BAEKJOON/CHOULOKY/16. 스택, 큐, 덱/28278.cpp
+++ BAEKJOON/CHOULOKY/16. 스택, 큐, 덱/28278.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 int main(void) {
 	cin.tie(0); ios::sync_with_stdio(false);
-	
+	int n; cin >> n;
+	stack<int> stk;
+	for(int i=0; i<n; i++) {
+		int num; cin >> num;
+		switch(num) {
+			case 1: {
+				int temp; cin >> temp;
+				stk.push(temp);
+				break;
+			}
+			case 2: {
+				if(stk.empty()) cout << -1;
+				else {
+					cout << stk.top();
+					stk.pop();
+				}
+				break;
+			}
+			case 3: {
+				cout << stk.size();
+				break;
+			}
+			case 4: {
+				if(stk.empty()) cout << 1;
+				else cout << 0;
+				break;
+			}
+			case 5: {
+				if(stk.empty()) cout << -1;
+				else cout << stk.top();
+				break;
+			}
+		}
+		if(num != 1) cout << '\n';
+	}
 }
