ca4d654cbfd95f30714d25c621295b791dd4374e
CHOULOKY <dlsgml5131@gmail.com>
2025-01-01 17:10:38 +0900

    CHOULOKY BAEKJOON
    25.01.01

--- BAEKJOON/CHOULOKY/28. 우선순위 큐/11279.cpp
+++ BAEKJOON/CHOULOKY/28. 우선순위 큐/11279.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
 
 int main(void) {
 	cin.tie(0); ios::sync_with_stdio(false);
 	
+	priority_queue<int, vector<int>, less<int>> pq;
+	
+	int n; cin >> n;
+	for(int i=0; i<n; i++) {
+		int num; cin >> num;
+		if(num == 0) {
+			if(pq.empty()) cout << 0;
+			else {
+				cout << pq.top();
+				pq.pop();
+			}
+			cout << '\n';
+		}
+		else {
+			pq.push(num);
+		}
+	}
 }
--- BAEKJOON/CHOULOKY/28. 우선순위 큐/11286.cpp
+++ BAEKJOON/CHOULOKY/28. 우선순위 큐/11286.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <queue>
+#include <vector>
+#include <cmath>
 using namespace std;
 
 int main(void) {
 	cin.tie(0); ios::sync_with_stdio(false);
 	
+	auto cmp = [](int a, int b) {
+		if(abs(a) == abs(b)) return a > b;
+		return abs(a) > abs(b);
+	};
+	priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
+	
+	int n; cin >> n;
+	for(int i=0; i<n; i++) {
+		int num; cin >> num;
+		if(num == 0) {
+			if(pq.empty()) cout << 0;
+			else {
+				cout << pq.top();
+				pq.pop();
+			}
+			cout << '\n';
+		}
+		else {
+			pq.push(num);
+		}
+	}
 }
